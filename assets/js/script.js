ef4c5ef160e2bc09c7dfb8ce130e6446ca7724a3
Ivan Logutov (VanishJr) <ilogutov04@gmail.com>
2024-06-26 18:20:32 +0300

    fixing bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let currentForecastData = null; // Переменная для хранения текущих данных прогноза
 
     // Инициализация карты
-    const map = L.map('map')
+    const map = L.map('map').setView([51.505, -0.09], 10);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
